fe49ad27bacb7d024e74dd7bc9784fd1cd94d697
Matt Melito <matt_melito@outlook>
2023-01-26 14:31:27 -0800

    Add Javascript branches for various language results and add error message.

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,55 @@ window.onload = function() {
   form.onsubmit = function(event) {
     event.preventDefault();
 
-    let ruby = document.getElementById("ruby");
-    ruby.setAttribute("class", "hidden");
-    let csharp = document.getElementById("csharp");
-    ruby.setAttribute("class", "hidden");
-    let javascript = document.getElementById("javascript");
-    ruby.setAttribute("class", "hidden");
-    let go = document.getElementById("go");
-    ruby.setAttribute("class", "hidden");
-    let python = document.getElementById("python");
-    ruby.setAttribute("class", "hidden");
-    let rust = document.getElementById("rust");
-    ruby.setAttribute("class", "hidden");
-    let swift = document.getElementById("swift");
-    ruby.setAttribute("class", "hidden");
-  }
-}
+    function hideResultsAndError() {
+      document.getElementById("error-message").setAttribute("class", "hidden");
+      document.getElementById("ruby").setAttribute("class", "hidden");
+      document.getElementById("csharp").setAttribute("class", "hidden");
+      document.getElementById("javascript").setAttribute("class", "hidden");
+      document.getElementById("go").setAttribute("class", "hidden");
+      document.getElementById("python").setAttribute("class", "hidden");
+      document.getElementById("rust").setAttribute("class", "hidden");
+      document.getElementById("swift").setAttribute("class", "hidden");
+      document.getElementById("html").setAttribute("class", "hidden");
+      document.getElementById("css").setAttribute("class", "hidden");
+      document.getElementById("java").setAttribute("class", "hidden");
+      document.getElementById("malboge").setAttribute("class", "hidden");
+    }
+    hideResultsAndError();
+
+    const exp = document.querySelector("input#experience").value;
+    const end = document.querySelector("input#frontorback").value;
+    const brain = document.querySelector("input#brain").value;
+    const writart = document.querySelector("input#writerartist").value;
+    const workplace = document.querySelector("input#workplace").value;
+    const gift = document.querySelector("input#gift").value;
+    
+    if (exp === "Beginner" && end === "front-end" && brain === "right-brained" && writart === "writer") {
+      document.getElementById("html").removeAttribute("class");
+    } else if (exp === "Beginner" && end === "front-end" && brain === "right-brained" && writart === "artist") {
+        document.getElementById("css").removeAttribute("class");
+    } else if (exp === "Beginner" && end === "front-end" && brain === "left-brained") {
+        document.getElementById("javascript").removeAttribute("class");
+    } else if (exp === "Beginner" && end === "back-end") {
+        document.getElementById("javascript").removeAttribute("class");
+    } else if (exp === "Professional" && gift === "bag of coffee" && workplace === "not sure") {
+        document.getElementById("java").removeAttribute("class");
+    } else if (exp === "Professional" && gift === "gemstone" && workplace === "not sure") {
+        document.getElementById("ruby").removeAttribute("class");
+    } else if (exp === "Professional" && gift === "snake" && workplace === "not sure") {
+        document.getElementById("python").removeAttribute("class");
+    } else if (exp === "Professional" && workplace === "Google") {
+      document.getElementById("go").removeAttribute("class");
+    } else if (exp === "Professional" && workplace === "Microsoft") {
+      document.getElementById("csharp").removeAttribute("class");
+    } else if (exp === "Professional" && (workplace === "Apple" || gift === "Taylor Swift vinyl album")) {
+        document.getElementById("swift").removeAttribute("class");
+    } else if (exp === "Professional" && (workplace === "Mozilla" || gift === "old car")) {
+      document.getElementById("rust").removeAttribute("class");
+    } else if (exp === "Master") {
+      document.getElementById("malboge").removeAttribute("class");
+    } else {
+      document.getElementById("error-message").removeAttribute("class");
+    }
+  };
+};
