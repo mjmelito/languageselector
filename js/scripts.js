1d59354f29a00599cc7f2356473d5cf71d1e13b4
Matt Melito <matt_melito@outlook>
2023-01-26 21:37:11 -0800

    Add photos for each language result and add JS to hide form when results show.

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ window.onload = function() {
       document.getElementById("html").setAttribute("class", "hidden");
       document.getElementById("css").setAttribute("class", "hidden");
       document.getElementById("java").setAttribute("class", "hidden");
-      document.getElementById("malboge").setAttribute("class", "hidden");
+      document.getElementById("malbolge").setAttribute("class", "hidden");
     }
     hideResultsAndError();
 
@@ -28,28 +28,52 @@ window.onload = function() {
     
     if (exp === "Beginner" && end === "front-end" && brain === "right-brained" && writeOrArt === "writer") {
       document.getElementById("html").removeAttribute("class");
+      document.getElementById("quiz").setAttribute("class", "hidden");
+      document.getElementById("warning").setAttribute("class", "hidden");
     } else if (exp === "Beginner" && end === "front-end" && brain === "right-brained" && writeOrArt === "artist") {
         document.getElementById("css").removeAttribute("class");
+        document.getElementById("quiz").setAttribute("class", "hidden");
+        document.getElementById("warning").setAttribute("class", "hidden");
     } else if (exp === "Beginner" && end === "front-end" && brain === "left-brained") {
         document.getElementById("javascript").removeAttribute("class");
+        document.getElementById("quiz").setAttribute("class", "hidden");
+        document.getElementById("warning").setAttribute("class", "hidden");
     } else if (exp === "Beginner" && end === "back-end") {
         document.getElementById("javascript").removeAttribute("class");
+        document.getElementById("quiz").setAttribute("class", "hidden");
+        document.getElementById("warning").setAttribute("class", "hidden");
     } else if (exp === "Professional" && gift === "bag of coffee" && workplace === "not sure") {
         document.getElementById("java").removeAttribute("class");
+        document.getElementById("quiz").setAttribute("class", "hidden");
+        document.getElementById("warning").setAttribute("class", "hidden");
     } else if (exp === "Professional" && gift === "gemstone" && workplace === "not sure") {
         document.getElementById("ruby").removeAttribute("class");
+        document.getElementById("quiz").setAttribute("class", "hidden");
+        document.getElementById("warning").setAttribute("class", "hidden");
     } else if (exp === "Professional" && gift === "snake" && workplace === "not sure") {
         document.getElementById("python").removeAttribute("class");
+        document.getElementById("quiz").setAttribute("class", "hidden");
+        document.getElementById("warning").setAttribute("class", "hidden");
     } else if (exp === "Professional" && workplace === "Google") {
       document.getElementById("go").removeAttribute("class");
+      document.getElementById("quiz").setAttribute("class", "hidden");
+      document.getElementById("warning").setAttribute("class", "hidden");
     } else if (exp === "Professional" && workplace === "Microsoft") {
       document.getElementById("csharp").removeAttribute("class");
+      document.getElementById("quiz").setAttribute("class", "hidden");
+      document.getElementById("warning").setAttribute("class", "hidden");
     } else if (exp === "Professional" && (workplace === "Apple" || gift === "Taylor Swift vinyl album")) {
         document.getElementById("swift").removeAttribute("class");
+        document.getElementById("quiz").setAttribute("class", "hidden");
+        document.getElementById("warning").setAttribute("class", "hidden");
     } else if (exp === "Professional" && (workplace === "Mozilla" || gift === "old car")) {
       document.getElementById("rust").removeAttribute("class");
+      document.getElementById("quiz").setAttribute("class", "hidden");
+      document.getElementById("warning").setAttribute("class", "hidden");
     } else if (exp === "Master") {
-      document.getElementById("malboge").removeAttribute("class");
+      document.getElementById("malbolge").removeAttribute("class");
+      document.getElementById("quiz").setAttribute("class", "hidden");
+      document.getElementById("warning").setAttribute("class", "hidden");
     } else {
       document.getElementById("error-message").removeAttribute("class");
     }
